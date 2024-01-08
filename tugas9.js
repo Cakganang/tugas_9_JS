cf9e9ef8928617f1de96b0adc0dffb3531deaf63
sonny faristrianto <sonnyf4r15@gmail.com>
2024-01-08 21:46:20 +0700

    tambah tugas9

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function nestedObject() {
+var mahasiswa ={
+    nama : "sonny",
+    jurusan: "informatika",
+    ipk : 3.5,
+}
+for (var x in mahasiswa){
+    console.log(mahasiswa[x]);
+}
+}
+
+nestedObject()
